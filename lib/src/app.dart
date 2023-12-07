db7386b12c1b8c825162162816f6882263deffd0
jsmile <jsmile@naver.com>
2023-12-07 17:21:24 +0900

    udemy starter pages

---
+++ lib/src/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class App extends ConsumerWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'GoRouter with Riverpod',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(fontSize: 24),
+          labelLarge: TextStyle(fontSize: 24),
+        ),
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
